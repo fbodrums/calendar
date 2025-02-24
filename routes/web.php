977c5a4c89f7806444194389dd776adc7a6b5825
Fabio Rafael <capitani18@exa.com.br>
2025-02-24 10:04:21 -0300

    Corrigindo rotas

--- routes/web.php
+++ routes/web.php
@@ -6,31 +6,37 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\Common\ExternalApisController;
 use App\Http\Controllers\UserController;
 
-// ROTAS DE AUTENTICAÇÃO
-Route::view('/', 'auth.login')->name('/');
-Route::view('login', 'auth.login')->name('login.view');
+Route::get('/', fn() => auth()->check() ? redirect()->route('contact.show.all') : view('auth.login'))->name('home');
 
-Route::get('logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('recover/{email}/{token}', [AuthController::class, 'showRecoverPassword'])->name('recover-password.view');
-Route::get('email/validate/{id}/{token}', [AuthController::class, 'validateEmail'])->name('validate-email');
+// ROTAS DE AUTENTICAÇÃO (Apenas para usuários não autenticados)
+Route::middleware('guest')->group(function () {
+    Route::view('login', 'auth.login')->name('login.view');
 
-Route::post('login', [AuthController::class, 'login'])->name('login');
-Route::post('signup', [AuthController::class, 'signup'])->name('signup');
-Route::post('forget-password', [AuthController::class, 'sendPasswordResetLink'])->name('forget-password');
-Route::post('recover-password', [AuthController::class, 'resetPassword'])->name('recover-password');
+    Route::get('recover/{email}/{token}', [AuthController::class, 'showRecoverPassword'])->name('recover-password.view');
+    Route::get('email/validate/{id}/{token}', [AuthController::class, 'validateEmail'])->name('validate-email');
 
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('signup', [AuthController::class, 'signup'])->name('signup');
+    Route::post('forget-password', [AuthController::class, 'sendPasswordResetLink'])->name('forget-password');
+    Route::post('recover-password', [AuthController::class, 'resetPassword'])->name('recover-password');
+});
 
-
+// ROTAS PROTEGIDAS POR AUTENTICAÇÃO
 Route::middleware('auth')->group(function () {
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+    // CONTATOS
     Route::view('contacts', 'contacts.index')->name('contact.show.all');
     Route::resource('contact', ContactController::class);
 
+    // USUÁRIOS
     Route::prefix('user')->group(function () {
-        Route::get('', [UserController::class, 'index'])->name('user.show');
-        Route::put('', [UserController::class, 'update'])->name('user.update');
-        Route::delete('', [UserController::class, 'destroy'])->name('user.destroy');
+        Route::get('/', [UserController::class, 'index'])->name('user.show');
+        Route::put('/', [UserController::class, 'update'])->name('user.update');
+        Route::delete('/', [UserController::class, 'destroy'])->name('user.destroy');
     });
 
+    // API EXTERNA (CEP, ENDEREÇO)
     Route::prefix('api')->group(function () {
         Route::get('cep/{cep}', [ExternalApisController::class, 'cep'])->name('cep');
         Route::get('address', [ExternalApisController::class, 'address'])->name('address');
